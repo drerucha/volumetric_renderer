c5fc2eec1849830c591b79fc3b8e3204298aed3e
Danny Rerucha <DannyRerucha@gmail.com>
2014-10-01 23:32:20 -0400

    Add glm library for linear algebra operations.

--- volumetric_renderer_project/volumetric_renderer_project/main.cpp
+++ volumetric_renderer_project/volumetric_renderer_project/main.cpp
@@ -1,10 +1,15 @@
 #pragma once
 
 #include <iostream>
+#include "../glm/glm.hpp"
+//#include "../glm/gtc/matrix_transform.hpp"
 
 int main(int argc, char** argv)
 {
+	glm::vec3 light_pos( 0.0f, 2.0f, 10.0f );
+
 	std::cout << "Hello world." << std::endl;
+	printf( "( %.2f, %.2f, %.2f )", light_pos.x, light_pos.y, light_pos.z );
 	std::cin.ignore();
 
 	return 0;
